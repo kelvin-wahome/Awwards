f338e4a1216dd1ad94d4d6ae5c3933143351f68a
blackrose <wahomekelving@gmail.com>
2019-03-18 18:29:29 +0300

    Added index url

--- award/views.py
+++ award/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.models import User
+
 from .models import Profile,Project
 from django.contrib.auth.decorators import login_required
 
