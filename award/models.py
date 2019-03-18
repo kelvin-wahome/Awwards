d1cf5b55e1ffad12c6f13924a5ef08f7e3654f2a
blackrose <wahomekelving@gmail.com>
2019-03-18 11:54:52 +0300

    Added Profile model

--- award/models.py
+++ award/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+  '''
+  class that contains user Profile properties
+  '''
+  profile_pic = models.ImageField(upload_to='images/',null=True, blank=True)
+  bio = models.TextField()
+  contact = models.IntegerField(blank=True, null=True,)
+  user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
