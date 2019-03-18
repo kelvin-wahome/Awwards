79dae72b1b2dabefa5f0d61d9557260af539c533
blackrose <wahomekelving@gmail.com>
2019-03-18 19:53:37 +0300

    Added update profile form

--- award/forms.py
+++ award/forms.py
@@ -8,8 +8,13 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        
+
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude =['posted_on','profile','user','rating',]
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
