ce0d36f7ead0bc976c85a9d77c05141aba145916
blackrose <wahomekelving@gmail.com>
2019-03-18 12:13:36 +0300

    Added a save project method

--- award/models.py
+++ award/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -62,3 +64,6 @@ class Project(models.Model):
   user = models.ForeignKey(User,on_delete=models.CASCADE,default="",blank=True,null=True)
   profile = models.ForeignKey(Profile,on_delete=models.CASCADE,default="",blank=True,null=True)
   rating = models.TextField()
+
+  def save_project(self):
+    self.save()
