faa54793ceef3120394af2f06fb57d9901317492
blackrose <wahomekelving@gmail.com>
2019-03-18 17:31:22 +0300

    Added view function to render the home page

--- award/views.py
+++ award/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Profile,Project
 
-# Create your views here.
+
+def index(request):
+  '''
+  view function that renders the homepage
+  '''
+  form=DesignForm()
+  projects = Project.get_posted_projects().order_by('-posted_on')
+
+  return render(request,'index.html',locals())
