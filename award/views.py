68aa73075ad4fd814eb73394a84648595f1c2e86
blackrose <wahomekelving@gmail.com>
2019-03-18 17:41:31 +0300

    Added base template

--- award/views.py
+++ award/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Profile,Project
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/accounts/login/')
 def index(request):
   '''
   view function that renders the homepage
--- awards/settings.py
+++ awards/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'bootstrap3',
     'award.apps.AwardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
