4d06582318301bfa07b8bebba72b497e111ddb22
blackrose <wahomekelving@gmail.com>
2019-03-18 18:35:27 +0300

    Added signup form class

--- award/views.py
+++ award/views.py
@@ -4,6 +4,30 @@ from django.contrib.auth.models import User
 from .models import Profile,Project
 from django.contrib.auth.decorators import login_required
 
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your Awwwards account.'
+            message = render_to_string('registration/activate_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 @login_required(login_url='/accounts/login/')
 def index(request):
--- awards/urls.py
+++ awards/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url,include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'',include('award.urls')),
+
 ]
