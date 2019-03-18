b9d90df21e586a75e4091d3477a8d7c746426bcd
blackrose <wahomekelving@gmail.com>
2019-03-18 20:51:01 +0300

    Added usability rating model

--- award/models.py
+++ award/models.py
@@ -118,8 +118,27 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-        
+
 class DesignRating(models.Model):
+    CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10')
+    )
+    project = models.ForeignKey(Project)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField(choices=CHOICES, default=0)
+
+class UsabilityRating(models.Model):
     CHOICES = (
         (1, '1'),
         (2, '2'),
